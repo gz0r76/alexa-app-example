021e94493c0746a5f72fad4cdac850e4fc29b07c
gz0r76 <sebastian.bialas@gmail.com>
2018-03-05 15:28:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ alexaApp.dictionary = { "names": ["matt", "joe", "bob", "bill", "mary", "jane", 
 alexaApp.intent("nameIntent", {
     "slots": { "NAME": "LITERAL" },
     "utterances": [
-      "my {name is|name's} {names|NAME}", "set my name to {names|NAME}"
+      "{names|NAME} my {name is|name's} ", "set my name to {names|NAME}"
     ]
   },
   function(request, response) {
